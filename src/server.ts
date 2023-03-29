2c058cef2524e0bd49994671ffcf4802f9096595
Gabriel Medrado <medradoogabriel@gmail.com>
2023-03-29 16:55:59 -0300

    Atualização

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello world!'
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
