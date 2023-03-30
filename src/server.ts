57bfe938d66c73b7dbe2898e30807bb6fce60f9a
Gabriel Medrado <medradoogabriel@gmail.com>
2023-03-30 18:11:28 -0300

    Atualização

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
